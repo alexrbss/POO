e3d31f458a383f9211622b57147bbd1f20c8b05d
alexrbss <alexandre.rbss@gmail.com>
2025-04-29 14:36:17 -0300

    ajuste de formatação

--- unidade-1/UnidadesDeVelocidade.java
+++ unidade-1/UnidadesDeVelocidade.java
@@ -1,11 +1,29 @@
+import java.util.Locale;
+import java.text.DecimalFormat;
+
 public class UnidadesDeVelocidade {
     public static void main(String[] arg) {
-        double kph = 0.0, mps;
+        Locale.setDefault(Locale.US);
 
-        System.out.print("K/H \tM/S");
+        double kph = 0.0, mps, mph, pps;
+
+        System.out.print("k/h \tm/s\t m/h\t p/s\n");
         while (kph <= 50.0) {
             mps = kph * 0.2778;
-            System.out.println(kph + "\t" + mps);
+            mph = kph * 0.6214;
+            pps = kph * 0.44704;
+            
+            // if (kph < 10) System.out.print("0");
+            // System.out.printf("%.2f\t", kph);
+            
+            // if (mps < 10) System.out.print("0");
+            // System.out.printf("%.2f\n", mps);
+            
+            System.out.print(new DecimalFormat("00.00").format(kph) + "\t");
+            System.out.print(new DecimalFormat("00.00").format(mps) + "\t");
+            System.out.print(new DecimalFormat("00.00").format(mph) + "\t");
+            System.out.println(new DecimalFormat("00.00").format(pps));
+
             kph+=0.5;
         }
         
